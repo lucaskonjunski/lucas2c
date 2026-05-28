21f984b974ced10ea657efc50e90c3414fd780eb
lucas2c <lucaskonjunskiivanchichen@gmail.com>
2026-05-28 10:54:51 -0300

    atividade01

---
+++ aula01/ex01.js
@@ -0,0 +1,6 @@
+let  Nome = "lucas";
+let idade = 16;
+let  cidade = "cascavel";
+let  estado = "parana";
+
+console.log (`meu nome é ${Nome} , tenho ${idade} , moro em ${cidade} no estado do ${estado}`);
